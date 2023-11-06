32033597582d1f960f015b71ac7a727297997295
oladayo ahmod <oladayoahmod112@gmail.com>
2023-11-06 02:23:43 -0800

    implement government api

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import {Government_provider} from '@/context/GovernanceContext'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Government_provider>
+          {children}
+        </Government_provider>
+      </body>
     </html>
   )
 }
